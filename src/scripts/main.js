d1bb00cb4b1762c573de2a9a2c5a7771edcc18ec
fromjkeee16 <fromjkeee16@gmail.com>
2025-03-05 12:30:17 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,36 @@
 'use strict';
 
-// write code here
+function parseSalary(salary) {
+  return +salary.replaceAll(/[$,.]/g, '');
+}
+
+function sortList(list) {
+  const items = [...list.children];
+
+  items.sort(
+    (a, b) => parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary),
+  );
+
+  return items;
+}
+
+function getEmployees(list) {
+  return [...list.children].map((element) => {
+    const { salary, position, age } = element.dataset;
+
+    return {
+      name: element.textContent,
+      position: position,
+      salary: parseSalary(salary),
+      age: +age,
+    };
+  });
+}
+
+const domList = document.querySelector('ul');
+
+const sortedList = sortList(domList);
+
+domList.append(...sortedList);
+
+getEmployees(domList);
