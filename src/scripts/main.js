5c39ace74af421046ea8372cdbf2679a6010d7de
fromjkeee16 <fromjkeee16@gmail.com>
2025-03-31 13:33:35 +0300

    fix ai problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,20 @@
+/* eslint-disable no-console */
 'use strict';
 
 function parseSalary(salary) {
-  return +salary.replaceAll(/[$,.]/g, '');
+  if (!salary) {
+    return 0;
+  }
+
+  return +salary.replaceAll(/[$,.]/g, '') || 0;
 }
 
 function sortList(list) {
-  const items = [...list.children];
+  if (!list) {
+    return [];
+  }
+
+  const items = [...list.children].filter((el) => el.dataset.salary);
 
   items.sort(
     (a, b) => parseSalary(b.dataset.salary) - parseSalary(a.dataset.salary),
@@ -15,22 +24,27 @@ function sortList(list) {
 }
 
 function getEmployees(list) {
+  if (!list) {
+    return [];
+  }
+
   return [...list.children].map((element) => {
-    const { salary, position, age } = element.dataset;
+    const { salary = '0', position = 'Unknown', age = '0' } = element.dataset;
 
     return {
-      name: element.textContent,
-      position: position,
+      name: element.textContent.trim(),
+      position,
       salary: parseSalary(salary),
-      age: +age,
+      age: +age || 0,
     };
   });
 }
 
 const domList = document.querySelector('ul');
 
-const sortedList = sortList(domList);
+if (domList) {
+  const sortedList = sortList(domList);
 
-domList.append(...sortedList);
-
-getEmployees(domList);
+  domList.append(...sortedList);
+  console.log(getEmployees(domList));
+}
